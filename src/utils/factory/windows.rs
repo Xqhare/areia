b4af364ffb7844d0500eb1600bd4e5440a2a93a7
Xqhare <xqhare@web.de>
2026-01-12 14:23:33 +0100

    add(factory/win): stubs for future

--- src/utils/factory/windows.rs
+++ src/utils/factory/windows.rs
@@ -14,10 +14,30 @@ pub fn runtime_dir() -> Option<PathBuf> {
     todo!(runtime_dir())
 }
 
-pub fn state_dir(home: PathBuf) -> PathBuf {
+pub fn state_dir(home: PathBuf) -> Option<PathBuf> {
     todo!(state_dir(home))
 }
 
-pub fn executable_dir(home: PathBuf) -> PathBuf {
+pub fn executable_dir(home: PathBuf) -> Option<PathBuf> {
     todo!(executable_dir(home))
+}
+
+pub fn config_local_dir(home: PathBuf) -> PathBuf {
+    config_dir(home)
+}
+
+pub fn data_local_dir(home: PathBuf) -> PathBuf {
+    data_dir(home)
+}
+
+pub fn preference_dir(home: PathBuf) -> PathBuf {
+    config_dir(home)
+}
+
+pub fn font_dir(home: PathBuf) -> PathBuf {
+    todo!(font_dir(home))
+}
+
+pub fn get_usr_dirs<P: Into<PathBuf>>(home: P) -> AreiaResult<HashMap<String, PathBuf>> {
+    todo!(get_usr_dirs(home))
 }
