d2e544a5895de9eb778826a63afa5799c229b5b7
Xqhare <xqhare@web.de>
2026-01-13 14:27:43 +0100

    feat(utils/hidden): start work on implementing hiding paths

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 mod utils;
 mod dirs;
 pub use dirs::{BaseDirs, UserDirs};
+pub use utils::Hidden;
 
 pub mod error;
 
--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,6 +1,6 @@
 mod ffi;
 use crate::error::AreiaResult;
-use std::path::PathBuf;
+use std::{ffi::OsStr, path::PathBuf};
 
 pub mod factory;
 
@@ -17,4 +17,38 @@ use windows as os;
 
 pub fn get_home() -> AreiaResult<PathBuf> {
     os::get_home()
+}
+
+pub trait Hidden {
+    fn is_hidden(&self) -> bool;
+    fn hide(&mut self) -> AreiaResult<()>;
+    fn unhide(&mut self) -> AreiaResult<()>;
+}
+
+impl Hidden for PathBuf {
+    /// Returns true if the path is hidden
+    ///
+    /// Every part of the path is checked - Returns true if any part is hidden
+    /// Does not check if a path exists
+    fn is_hidden(&self) -> bool {
+        self.components().any(|c| is_component_hidden(c.as_os_str()))
+    }
+
+    /// Hides the path
+    ///
+    /// Checks if any component of the path is already hidden.
+    /// If not, the last directory contained in the path is hidden.
+    ///
+    /// Returns an error if the path does not exist
+    fn hide(&mut self) -> AreiaResult<()> {
+        todo!()
+    }
+
+    fn unhide(&mut self) -> AreiaResult<()> {
+        todo!()
+    }
+}
+
+fn is_component_hidden(path: &OsStr) -> bool {
+    todo!(())
 }
