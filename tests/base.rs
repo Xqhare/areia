7f90892e99eefb6e80d5a53efcf5ec5107ebe9fd
Xqhare <xqhare@web.de>
2026-01-13 12:12:17 +0100

    tests(integration): add basic integration tests

---
+++ tests/base.rs
@@ -0,0 +1,96 @@
+use areia::BaseDirs;
+use std::path::PathBuf;
+
+#[test]
+fn create_new_base() {
+    let new_base = BaseDirs::new();
+    assert!(new_base.is_ok());
+}
+
+#[test]
+#[cfg(target_os = "linux")]
+fn all_paths_present() {
+    let base = BaseDirs::new();
+    assert!(base.is_ok());
+    let base = base.unwrap();
+
+    assert!(base.home_dir().is_absolute());
+    assert_ne!(base.home_dir(), &PathBuf::from(""));
+    assert_ne!(base.home_dir(), &PathBuf::from("/"));
+    assert!(base.cache_dir().is_absolute());
+    assert_ne!(base.cache_dir(), &PathBuf::from(""));
+    assert!(base.config_dir().is_absolute());
+    assert_ne!(base.config_dir(), &PathBuf::from(""));
+    assert!(base.config_local_dir().is_absolute());
+    assert_ne!(base.config_local_dir(), &PathBuf::from(""));
+    assert!(base.data_dir().is_absolute());
+    assert_ne!(base.data_dir(), &PathBuf::from(""));
+    assert!(base.data_local_dir().is_absolute());
+    assert_ne!(base.data_local_dir(), &PathBuf::from(""));
+    assert!(base.executable_dir().is_some());
+    assert!(base.executable_dir().unwrap().is_absolute());
+    assert_ne!(base.executable_dir().unwrap(), &PathBuf::from(""));
+    assert!(base.preference_dir().is_absolute());
+    assert_ne!(base.preference_dir(), &PathBuf::from(""));
+    // Runtime may be set or not
+    if base.runtime_dir().is_some() {
+        assert!(base.runtime_dir().unwrap().is_absolute());
+        assert_ne!(base.runtime_dir().unwrap(), &PathBuf::from(""));
+    }
+    assert!(base.state_dir().is_some());
+    assert!(base.state_dir().unwrap().is_absolute());
+    assert_ne!(base.state_dir().unwrap(), &PathBuf::from(""));
+}
+
+#[test]
+#[cfg(target_os = "macos")]
+fn all_paths_present() {
+    let base = BaseDirs::new();
+    assert!(base.is_ok());
+    let base = base.unwrap();
+
+    assert!(base.home_dir().is_absolute());
+    assert_ne!(base.home_dir(), &PathBuf::from(""));
+    assert_ne!(base.home_dir(), &PathBuf::from("/"));
+    assert!(base.cache_dir().is_absolute());
+    assert_ne!(base.cache_dir(), &PathBuf::from(""));
+    assert!(base.config_dir().is_absolute());
+    assert_ne!(base.config_dir(), &PathBuf::from(""));
+    assert!(base.config_local_dir().is_absolute());
+    assert_ne!(base.config_local_dir(), &PathBuf::from(""));
+    assert!(base.data_dir().is_absolute());
+    assert_ne!(base.data_dir(), &PathBuf::from(""));
+    assert!(base.data_local_dir().is_absolute());
+    assert_ne!(base.data_local_dir(), &PathBuf::from(""));
+    assert!(base.executable_dir().is_none());
+    assert!(base.preference_dir().is_absolute());
+    assert_ne!(base.preference_dir(), &PathBuf::from(""));
+    assert!(base.runtime_dir().is_none());
+    assert!(base.state_dir().is_none());
+}
+
+#[test]
+#[cfg(target_os = "windows")]
+fn all_paths_present() {
+    let base = BaseDirs::new();
+    assert!(base.is_ok());
+    let base = base.unwrap();
+
+    assert!(base.home_dir().is_absolute());
+    assert_ne!(base.home_dir(), &PathBuf::from(""));
+    assert!(base.cache_dir().is_absolute());
+    assert_ne!(base.cache_dir(), &PathBuf::from(""));
+    assert!(base.config_dir().is_absolute());
+    assert_ne!(base.config_dir(), &PathBuf::from(""));
+    assert!(base.config_local_dir().is_absolute());
+    assert_ne!(base.config_local_dir(), &PathBuf::from(""));
+    assert!(base.data_dir().is_absolute());
+    assert_ne!(base.data_dir(), &PathBuf::from(""));
+    assert!(base.data_local_dir().is_absolute());
+    assert_ne!(base.data_local_dir(), &PathBuf::from(""));
+    assert!(base.executable_dir().is_none());
+    assert!(base.preference_dir().is_absolute());
+    assert_ne!(base.preference_dir(), &PathBuf::from(""));
+    assert!(base.runtime_dir().is_none());
+    assert!(base.state_dir().is_none());
+}
---
+++ tests/user.rs
@@ -0,0 +1,111 @@
+use areia::UserDirs;
+use std::path::PathBuf;
+
+#[test]
+fn create_new_user() {
+    let new_user = UserDirs::new();
+    assert!(new_user.is_ok());
+}
+
+#[test]
+#[cfg(target_os = "linux")]
+fn all_paths_present() {
+    let user = UserDirs::new();
+    assert!(user.is_ok());
+    let user = user.unwrap();
+
+    assert!(user.home_dir().is_absolute());
+    // All linux user directories may not exist, with the exception of `Home`, `Fonts`
+    if user.audio_dir().is_some() {
+        assert!(user.audio_dir().as_ref().unwrap().is_absolute());
+        assert_ne!(user.audio_dir().as_ref().unwrap(), &PathBuf::from(""));
+    }
+    if user.desktop_dir().is_some() {
+        assert!(user.desktop_dir().as_ref().unwrap().is_absolute());
+        assert_ne!(user.desktop_dir().as_ref().unwrap(), &PathBuf::from(""));
+    }
+    if user.document_dir().is_some() {
+        assert!(user.document_dir().as_ref().unwrap().is_absolute());
+        assert_ne!(user.document_dir().as_ref().unwrap(), &PathBuf::from(""));
+    }
+    if user.download_dir().is_some() {
+        assert!(user.download_dir().as_ref().unwrap().is_absolute());
+        assert_ne!(user.download_dir().as_ref().unwrap(), &PathBuf::from(""));
+    }
+    assert!(user.font_dir().is_some());
+    assert!(user.font_dir().as_ref().unwrap().is_absolute());
+    assert_ne!(user.font_dir().as_ref().unwrap(), &PathBuf::from(""));
+    if user.picture_dir().is_some() {
+        assert!(user.picture_dir().as_ref().unwrap().is_absolute());
+        assert_ne!(user.picture_dir().as_ref().unwrap(), &PathBuf::from(""));
+    }
+    if user.public_dir().is_some() {
+        assert!(user.public_dir().as_ref().unwrap().is_absolute());
+        assert_ne!(user.public_dir().as_ref().unwrap(), &PathBuf::from(""));
+    }
+    if user.template_dir().is_some() {
+        assert!(user.template_dir().as_ref().unwrap().is_absolute());
+        assert_ne!(user.template_dir().as_ref().unwrap(), &PathBuf::from(""));
+    }
+    if user.video_dir().is_some() {
+        assert!(user.video_dir().as_ref().unwrap().is_absolute());
+        assert_ne!(user.video_dir().as_ref().unwrap(), &PathBuf::from(""));
+    }
+}
+
+#[test]
+#[cfg(target_os = "macos")]
+fn all_paths_present() {
+    let user = UserDirs::new();
+    assert!(user.is_ok());
+    let user = user.unwrap();
+
+    assert!(user.home_dir().is_absolute());
+    // In contrast to linux, only templates will never exists, all others always will
+    assert!(user.audio_dir().is_some());
+    assert_ne!(user.audio_dir().as_ref().unwrap(), &PathBuf::from(""));
+    assert!(user.desktop_dir().is_some());
+    assert_ne!(user.desktop_dir().as_ref().unwrap(), &PathBuf::from(""));
+    assert!(user.document_dir().is_some());
+    assert_ne!(user.document_dir().as_ref().unwrap(), &PathBuf::from(""));
+    assert!(user.download_dir().is_some());
+    assert_ne!(user.download_dir().as_ref().unwrap(), &PathBuf::from(""));
+    assert!(user.font_dir().is_some());
+    assert_ne!(user.font_dir().as_ref().unwrap(), &PathBuf::from(""));
+    assert!(user.picture_dir().is_some());
+    assert_ne!(user.picture_dir().as_ref().unwrap(), &PathBuf::from(""));
+    assert!(user.public_dir().is_some());
+    assert_ne!(user.public_dir().as_ref().unwrap(), &PathBuf::from(""));
+    assert!(user.template_dir().is_none());
+    assert!(user.video_dir().is_some());
+    assert_ne!(user.video_dir().as_ref().unwrap(), &PathBuf::from(""));
+
+}
+
+#[test]
+#[cfg(target_os = "macos")]
+fn all_paths_present() {
+    let user = UserDirs::new();
+    assert!(user.is_ok());
+    let user = user.unwrap();
+
+    assert!(user.home_dir().is_absolute());
+    // In contrast to mac, only fonts will never exists, all others always will
+    assert!(user.audio_dir().is_some());
+    assert_ne!(user.audio_dir().as_ref().unwrap(), &PathBuf::from(""));
+    assert!(user.desktop_dir().is_some());
+    assert_ne!(user.desktop_dir().as_ref().unwrap(), &PathBuf::from(""));
+    assert!(user.document_dir().is_some());
+    assert_ne!(user.document_dir().as_ref().unwrap(), &PathBuf::from(""));
+    assert!(user.download_dir().is_some());
+    assert_ne!(user.download_dir().as_ref().unwrap(), &PathBuf::from(""));
+    assert!(user.font_dir().is_none());
+    assert!(user.picture_dir().is_some());
+    assert_ne!(user.picture_dir().as_ref().unwrap(), &PathBuf::from(""));
+    assert!(user.public_dir().is_some());
+    assert_ne!(user.public_dir().as_ref().unwrap(), &PathBuf::from(""));
+    assert!(user.template_dir().is_some());
+    assert_ne!(user.template_dir().as_ref().unwrap(), &PathBuf::from(""));
+    assert!(user.video_dir().is_some());
+    assert_ne!(user.video_dir().as_ref().unwrap(), &PathBuf::from(""));
+}
