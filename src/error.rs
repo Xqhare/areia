d1c865b79b3f9a8cad3a80afa11cc073b2839563
Xqhare <xqhare@web.de>
2026-01-13 14:26:27 +0100

    add(error): Mac errors

--- src/error.rs
+++ src/error.rs
@@ -2,12 +2,14 @@
 pub type AreiaResult<T> = Result<T, AreiaError>;
 
 type WinErrString = String;
+type MacErrString = String;
 
 #[derive(Debug)]
 pub enum AreiaError {
     CantGetHomeDir,
     IoError(std::io::Error),
     WindowsError(WinErrString),
+    MacError(MacErrString),
 }
 
 impl std::fmt::Display for AreiaError {
@@ -16,6 +18,7 @@ impl std::fmt::Display for AreiaError {
             AreiaError::CantGetHomeDir => write!(f, "Can't get home directory"),
             AreiaError::IoError(err) => write!(f, "IO error: {}", err),
             AreiaError::WindowsError(err) => write!(f, "Windows error. \n Unable to get: {}", err),
+            AreiaError::MacError(err) => write!(f, "Mac error. \n Unable to get: {}", err),
         }
     }
 }
