d5b2aa2bebe11216aa329cc5ae6227690d0cb9d7
Xqhare <xqhare@web.de>
2026-01-16 14:53:20 +0100

    doc(traits::super_hidden): is_superhidden doc

--- src/hidden/super_hide.rs
+++ src/hidden/super_hide.rs
@@ -1,21 +1,48 @@
 use std::path::PathBuf;
 
-use crate::error::AreiaResult;
+use crate::error::{AreiaError, AreiaResult};
 
 use super::SuperHidden;
 
 
 impl SuperHidden for PathBuf {
+    /// Checks if any component of the path is `super hidden`.\
+    /// The path must point to an existing file or directory.\
+    /// This concept only applies to MacOS and Windows.
+    ///
+    /// `super hidden` is a file or directory that is hidden, as achieved with using `.hide()` provided by `areia`, and marked with a platform specific attribute or flag.
+    ///
+    /// # Platform specific behaviour
+    /// 
+    /// ## Unix
+    ///
+    /// ### Linux
+    ///
+    /// Always returns `AreiaError::SuperHidingNotSupported`.
+    ///
     /// ### MacOS
     ///
-    /// After dotfile checking, also checks for file attributes if path points to an existing
-    /// file or directory.
-    /// Returns `true` if any component is hidden.
+    /// Returns `true` if any component of the path is hidden (`.` prefix) and has the `hidden` flag set.
     ///
-    fn is_super_hidden(&self) -> bool {
-        todo!()
+    /// ## Windows
+    ///
+    /// Only the file or folder pointed to by the path is checked.\
+    /// Returns `true` if the attributes `Hidden` and `System` are set.
+    ///
+    /// # Errors
+    /// 
+    /// Errors if the path does not exist or an OS error occurs.
+    fn is_super_hidden(&self) -> AreiaResult<bool> {
+        if cfg!(target_os = "linux") {
+            Err(AreiaError::SuperHidingNotSupported("No super hiding on Linux".to_string()))
+        } else {
+            todo!()
+        }
     }
 
+    /// Super hides the file or directory pointed to by the path.\
+    /// The path must point to an existing file or directory.\
+    /// This concept only applies to MacOS and Windows.
     fn super_hide(&mut self) -> AreiaResult<PathBuf> {
         todo!()
     }
