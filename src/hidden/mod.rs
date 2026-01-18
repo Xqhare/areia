33651c21960aeb7b4c6e5c0d9125f7ebf552c394
Xqhare <xqhare@web.de>
2026-01-18 21:03:39 +0100

    doc(Hidden&SuperHidden): add examples to trait doc

--- src/hidden/mod.rs
+++ src/hidden/mod.rs
@@ -21,7 +21,27 @@ pub trait Hidden {
     /// Checks for the hidden attribute on the file or folder pointed to with the path.
     /// Returns `true` if the file or folder is hidden.
     /// Returns `false` if the file or folder is not hidden.
-    /// SPECIAL: Returns `false` if the path does not exist or an error occurs.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use areia::Hidden;
+    /// use std::path::PathBuf;
+    /// let mut path = PathBuf::from("is_hidden.tmp");
+    /// assert!(!path.is_hidden().unwrap());
+    /// if cfg!(unix) {
+    ///     let hidden_path = path.hide();
+    ///     assert!(hidden_path.is_ok());
+    ///     assert!(hidden_path.as_ref().unwrap().is_hidden().unwrap());
+    ///     # if hidden_path.as_ref().unwrap().exists() {
+    ///         # std::fs::remove_file(hidden_path.unwrap()).unwrap();
+    ///     # }
+    /// }
+    /// ```
+    ///
+    /// # Errors
+    ///
+    /// Errors if the path does not exist (Windows only) or an OS error occurs.
     fn is_hidden(&self) -> AreiaResult<bool>;
     /// Hides the path
     ///
@@ -48,6 +68,22 @@ pub trait Hidden {
     /// If the path exists, the file or folder is moved to be hidden.
     /// If the path does not exist, it is created.
     /// This is done using 'atomic move' - ensuring no data is lost.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use areia::Hidden;
+    /// use std::path::PathBuf;
+    /// let mut path = PathBuf::from("hide.tmp");
+    /// assert!(!path.is_hidden().unwrap());
+    /// assert!(std::fs::File::create(path.clone()).is_ok());
+    /// let hidden_path = path.hide();
+    /// assert!(hidden_path.is_ok());
+    /// assert!(hidden_path.as_ref().unwrap().is_hidden().unwrap());
+    /// # if hidden_path.as_ref().unwrap().exists() {
+    ///     # std::fs::remove_file(hidden_path.unwrap()).unwrap();
+    /// # }
+    /// ```
     ///
     /// # Errors
     ///
@@ -81,11 +117,48 @@ pub trait Hidden {
     /// This is done using 'atomic move' - ensuring no data is lost.
     /// If the path does not exist, an error is returned.
     ///
+    /// # Example
+    ///
+    /// ```
+    /// use areia::Hidden;
+    /// use std::path::PathBuf;
+    /// let mut path = PathBuf::from(".hidden.tmp");
+    /// assert!(path.is_hidden().unwrap());
+    /// assert!(std::fs::File::create(path.clone()).is_ok());
+    /// let unhidden_path = path.unhide();
+    /// assert!(unhidden_path.is_ok());
+    /// assert!(!unhidden_path.as_ref().unwrap().is_hidden().unwrap());
+    /// # if unhidden_path.as_ref().unwrap().exists() {
+    ///     # std::fs::remove_file(unhidden_path.unwrap()).unwrap();
+    /// # }
+    /// ```
+    ///
     /// # Errors
     ///
     /// Errors if the program has insufficient permissions to move the file or folder.
     fn unhide(&mut self) -> AreiaResult<PathBuf>;
-    // Like hide, but returns only the hidden path - no file creation whatsoever
+    /// Like hide, but returns only the hidden path - no file creation whatsoever.
+    ///
+    /// In contrast to `hide()` this does not need a mutable `PathBuf`
+    ///
+    /// # Platform specific behaviour
+    ///
+    /// See `hide()`.
+    ///
+    /// # Example
+    /// ```
+    /// use areia::Hidden;
+    /// use std::path::PathBuf;
+    /// let path = PathBuf::from("hide.tmp");
+    /// assert!(!path.is_hidden().unwrap());
+    /// let hidden_path = path.into_hidden_path();
+    /// assert!(hidden_path.is_ok());
+    /// assert!(hidden_path.as_ref().unwrap().is_hidden().unwrap());
+    /// ```
+    ///
+    /// # Errors
+    ///
+    /// Errors if the program has insufficient permissions to move the file or folder.
     fn into_hidden_path(self) -> AreiaResult<PathBuf>;
 }
 
@@ -112,6 +185,18 @@ pub trait SuperHidden {
     ///
     /// Only the file or folder pointed to by the path is checked.
     /// Returns `true` if the attributes `Hidden` and `System` are set.
+    /// # Example
+    /// ```
+    /// use areia::SuperHidden;
+    /// use std::path::PathBuf;
+    /// if cfg!(not(target_os = "linux")) {
+    ///     let path = PathBuf::from(".hidden.tmp");
+    ///     assert!(!path.is_super_hidden().unwrap());
+    ///     # if path.exists() {
+    ///         # std::fs::remove_file(path).unwrap();
+    ///     # }
+    /// }
+    /// ```
     ///
     /// # Errors
     ///
@@ -141,6 +226,22 @@ pub trait SuperHidden {
     /// Does nothing if the file or directory is already super hidden.
     /// Otherwise, the file or directory is hidden and marked with the `Hidden` and `System` attributes.
     ///
+    /// # Example
+    /// ```
+    /// use areia::SuperHidden;
+    /// use std::path::PathBuf;
+    /// if cfg!(not(target_os = "linux")) {
+    ///     let mut path = PathBuf::from("to_hide.tmp");
+    ///     assert!(!path.is_super_hidden().unwrap());
+    ///     let super_hidden_path = path.super_hide();
+    ///     assert!(super_hidden_path.is_ok());
+    ///     assert!(super_hidden_path.as_ref().unwrap().is_super_hidden().unwrap());
+    ///     # if super_hidden_path.as_ref().unwrap().exists() {
+    ///         # std::fs::remove_file(super_hidden_path.unwrap()).unwrap();
+    ///     # }
+    /// }
+    /// ```
+    ///
     /// # Errors
     ///
     /// Errors if the path does not exist or an OS error occurs.
@@ -169,6 +270,24 @@ pub trait SuperHidden {
     ///
     /// Does nothing if the file or directory is not super hidden.
     /// Otherwise, the file or directory is unhidden and unmarked with the `Hidden` and `System` attributes.
+    ///
+    /// # Example
+    /// ```
+    /// use areia::SuperHidden;
+    /// use std::path::PathBuf;
+    /// if cfg!(not(target_os = "linux")) {
+    ///     let mut path = PathBuf::from(".hidden.tmp");
+    ///     let super_hidden_path = path.super_hide();
+    ///     assert!(super_hidden_path.is_ok());
+    ///     assert!(super_hidden_path.as_ref().unwrap().is_super_hidden().unwrap());
+    ///     let unhidden_path = super_hidden_path.as_ref().unwrap().super_unhide();
+    ///     assert!(unhidden_path.is_ok());
+    ///     assert!(!unhidden_path.as_ref().unwrap().is_super_hidden().unwrap());
+    ///     # if unhidden_path.as_ref().unwrap().exists() {
+    ///         # std::fs::remove_file(unhidden_path.unwrap()).unwrap();
+    ///     # }
+    /// }
+    /// ```
     ///
     /// # Errors
     ///
