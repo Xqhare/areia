9a8c35dd3632c850abc0b7b6cf06401d693875f8
Xqhare <xqhare@web.de>
2026-01-19 11:48:57 +0100

    chore(autos): move auto_deletor & creator into its own file

--- src/lib.rs
+++ src/lib.rs
@@ -3,87 +3,15 @@
 mod dirs;
 mod hidden;
 mod utils;
+mod autos;
 
 pub mod error;
 
-use error::{AreiaError, AreiaResult};
-use std::path::PathBuf;
-use utils::{create_all_dir_with_file, delete_all_dir_with_files};
+// API
 
+// Directories
 pub use dirs::{BaseDirs, UserDirs};
+// Traits
 pub use hidden::{Hidden, SuperHidden};
-
-/// Convenience function to create a directory with a file in it.
-/// All parent directories are created if they do not exist.
-///
-/// # Example
-///
-/// ```
-/// # use areia::auto_deletor;
-/// use areia::auto_creator;
-/// use std::path::PathBuf;
-/// let path = PathBuf::from("test_dir/test_file.txt");
-/// assert!(auto_creator(&path).is_ok());
-/// assert!(&path.exists());
-/// # assert!(auto_deletor(&path).is_ok());
-/// ```
-///
-/// # Errors
-///
-/// Errors if the directory cannot be created and returns the OS error.
-pub fn auto_creator<P: Into<PathBuf>>(path: P) -> AreiaResult<()> {
-    let path = path.into();
-    create_all_dir_with_file(&path)
-}
-
-/// Convenience function to delete a directory with files in it.
-///
-/// ! USE WITH CAUTION !
-///
-/// All parent directories and any contained files are deleted if they exist.
-/// If only a file is passed in, all files and directories inside the same directory are deleted.
-///
-/// # Example
-///
-/// Given this structure:
-///
-/// parent/
-/// ├── child/
-/// │   ├── a.file
-/// │   └── b.file
-/// ├── child2/
-/// │   └── c.file
-/// ├── another.file
-/// └── my.file
-///
-/// If `auto_deletor(child/)` (or `auto_deletor(child/a.file)`) is called, `a.file`, `b.file` and it's parent directory `child` are
-/// deleted, resulting in:
-///
-/// parent/
-/// ├── child2/
-/// │   └── c.file
-/// ├── another.file
-/// └── my.file
-///
-/// By calling `auto_deletor(parent/my.file)` all files and directories in `parent/` are deleted
-/// alongisde `parent/` itself.
-///
-/// # Usage Example
-///
-/// ```
-/// use areia::{auto_creator, auto_deletor};
-/// use std::path::PathBuf;
-/// let path = PathBuf::from("testing_dir/test_file.txt");
-/// assert!(auto_creator(&path).is_ok());
-/// assert!(&path.exists());
-/// assert!(auto_deletor(&path).is_ok());
-/// assert!(!&path.exists());
-/// ```
-///
-/// # Errors
-///
-/// Errors if the directory cannot be deleted (e.g. does not exist) and returns the OS error.
-pub fn auto_deletor<P: Into<PathBuf>>(path: P) -> AreiaResult<()> {
-    let path = path.into();
-    delete_all_dir_with_files(&path)
-}
+// Utilities
+pub use autos::{auto_creator, auto_deletor};
