a67ce9f8887a13f8de9836e03767d5dddb72909b
Xqhare <xqhare@web.de>
2026-03-28 05:27:05 +0100

    fix test

--- tests/user.rs
+++ tests/user.rs
@@ -1,6 +1,7 @@
 use areia::UserDirs;
 
 #[test]
+#[ignore = "CI does not have a `user-dirs.dirs` file"]
 fn create_new_user() {
     let new_user = UserDirs::new();
     assert!(new_user.is_ok());
@@ -8,49 +9,48 @@ fn create_new_user() {
 
 #[test]
 #[cfg(target_os = "linux")]
+#[ignore = "CI does not have a `user-dirs.dirs` file"]
 fn all_paths_present() {
     let user = UserDirs::new();
-    // CI on the home-lab does not have a `user-dirs.dirs` file
-    if user.is_ok() {
-        let user = user.unwrap();
+    assert!(user.is_ok());
+    let user = user.unwrap();
 
-        assert!(user.home_dir().is_absolute());
-        // All linux user directories may not exist, with the exception of `Home`, `Fonts`
-        if user.audio_dir().is_some() {
-            assert!(user.audio_dir().as_ref().unwrap().is_absolute());
-            assert!(user.audio_dir().as_ref().unwrap().is_dir());
-        }
-        if user.desktop_dir().is_some() {
-            assert!(user.desktop_dir().as_ref().unwrap().is_absolute());
-            assert!(user.desktop_dir().as_ref().unwrap().is_dir());
-        }
-        if user.document_dir().is_some() {
-            assert!(user.document_dir().as_ref().unwrap().is_absolute());
-            assert!(user.document_dir().as_ref().unwrap().is_dir());
-        }
-        if user.download_dir().is_some() {
-            assert!(user.download_dir().as_ref().unwrap().is_absolute());
-            assert!(user.download_dir().as_ref().unwrap().is_dir());
-        }
-        assert!(user.font_dir().is_some());
-        assert!(user.font_dir().as_ref().unwrap().is_absolute());
-        assert!(user.font_dir().as_ref().unwrap().is_dir());
-        if user.picture_dir().is_some() {
-            assert!(user.picture_dir().as_ref().unwrap().is_absolute());
-            assert!(user.picture_dir().as_ref().unwrap().is_dir());
-        }
-        if user.public_dir().is_some() {
-            assert!(user.public_dir().as_ref().unwrap().is_absolute());
-            assert!(user.public_dir().as_ref().unwrap().is_dir());
-        }
-        if user.template_dir().is_some() {
-            assert!(user.template_dir().as_ref().unwrap().is_absolute());
-            assert!(user.template_dir().as_ref().unwrap().is_dir());
-        }
-        if user.video_dir().is_some() {
-            assert!(user.video_dir().as_ref().unwrap().is_absolute());
-            assert!(user.video_dir().as_ref().unwrap().is_dir());
-        }
+    assert!(user.home_dir().is_absolute());
+    // All linux user directories may not exist, with the exception of `Home`, `Fonts`
+    if user.audio_dir().is_some() {
+        assert!(user.audio_dir().as_ref().unwrap().is_absolute());
+        assert!(user.audio_dir().as_ref().unwrap().is_dir());
+    }
+    if user.desktop_dir().is_some() {
+        assert!(user.desktop_dir().as_ref().unwrap().is_absolute());
+        assert!(user.desktop_dir().as_ref().unwrap().is_dir());
+    }
+    if user.document_dir().is_some() {
+        assert!(user.document_dir().as_ref().unwrap().is_absolute());
+        assert!(user.document_dir().as_ref().unwrap().is_dir());
+    }
+    if user.download_dir().is_some() {
+        assert!(user.download_dir().as_ref().unwrap().is_absolute());
+        assert!(user.download_dir().as_ref().unwrap().is_dir());
+    }
+    assert!(user.font_dir().is_some());
+    assert!(user.font_dir().as_ref().unwrap().is_absolute());
+    assert!(user.font_dir().as_ref().unwrap().is_dir());
+    if user.picture_dir().is_some() {
+        assert!(user.picture_dir().as_ref().unwrap().is_absolute());
+        assert!(user.picture_dir().as_ref().unwrap().is_dir());
+    }
+    if user.public_dir().is_some() {
+        assert!(user.public_dir().as_ref().unwrap().is_absolute());
+        assert!(user.public_dir().as_ref().unwrap().is_dir());
+    }
+    if user.template_dir().is_some() {
+        assert!(user.template_dir().as_ref().unwrap().is_absolute());
+        assert!(user.template_dir().as_ref().unwrap().is_dir());
+    }
+    if user.video_dir().is_some() {
+        assert!(user.video_dir().as_ref().unwrap().is_absolute());
+        assert!(user.video_dir().as_ref().unwrap().is_dir());
     }
 }
 
